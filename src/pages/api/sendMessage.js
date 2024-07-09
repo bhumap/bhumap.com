6de2aa0801bea959d4e74ed189624955937496f7
md shamsher <mdsamsher530@gmail.com>
2024-07-09 18:39:03 +0530

    added

--- src/pages/api/sendMessage.js
+++ src/pages/api/sendMessage.js
@@ -1,20 +1,20 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { fullName, email, phoneNumber, message } = req.body;
 
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: "gmail",
       auth: {
-        user: 'mdsamsher530@gmail.com',
-        pass: 'zsty rfnx chin ehxn', // Use app-specific password or environment variable
+        user: "mdsamsher530@gmail.com",
+        pass: "cixw lxuz yczy vsor", 
       },
     });
 
     const mailOptions = {
       from: email,
-      to: 'mdsamsher530@gmail.com',
+      to: "mdsamsher530@gmail.com",
       subject: `Message from ${fullName}`,
       html: `
         <h1>Message from ${fullName}</h1>
@@ -29,11 +29,11 @@ export default async function handler(req, res) {
       await transporter.sendMail(mailOptions);
       res.status(200).json({ success: true });
     } catch (error) {
-      console.error('Error sending email:', error);
+      console.error("Error sending email:", error);
       res.status(500).json({ success: false, error: error.message });
     }
   } else {
-    res.setHeader('Allow', ['POST']);
+    res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
