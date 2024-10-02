06b0504932bd21dc93877ee879cc26ed336be7d6
vishal.singhtech@niyotail.com <vishal.singhtech@niyotail.com>
2024-10-02 16:03:31 +0530

    refral and earn

--- src/backend/models/users.js
+++ src/backend/models/users.js
@@ -61,6 +61,14 @@ const userSchema = new mongoose.Schema({
   otp:{
     value:Number,
     exexpirationTime:Date
+  },
+  refral_code: {
+    type: String,
+    required:true,
+    unique: true
+  },
+  referred_by: {
+    type: String
   }
 });
 
--- src/pages/api/auth/register/index.js
+++ src/pages/api/auth/register/index.js
@@ -1,17 +1,49 @@
 import dbConnect from "@/src/backend/config/dbConnect";
 import UsersModal from "@/src/backend/models/users";
+import RewardsModal from "@/src/backend/models/rewards";
 import bcrypt from "bcrypt";
 import validator from 'validator'
 import { serialize } from "cookie";
 import { GenAccessToken } from "@/src/backend/helpers/jwt";
+import generateRandomCode from "@/src/backend/helpers/GenerateRandomCode"; 
+import { StatusCodes } from 'http-status-codes';
+
+async function generateUniqueToken(length=6) {
+  const token = generateRandomCode(length);
+
+  const isAlreadyExist = await UsersModal.findOne({refral_code: token});
+
+  if (isAlreadyExist) {
+    generateRandomCode(length);
+  }
+
+  return token;
+}
+
+async function validateRefralCode(refral_code) {
+  const user = await UsersModal.findOne({refral_code});
+
+  if (!user) return {user: null, invalid: true};
+
+  return {user, invalid: false};
+}
 
 export default async function handler(req, res) {
   await dbConnect();
 
   try {
+    const {userType, refral_code} = req.body;
+    
+    if (String(userType).toLowerCase() == 'admin') {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: "Invalid request",
+      });
+      return
+    }
 
     if (!req.body.username) {
-      res.status(400).json({
+      res.status(StatusCodes.BAD_REQUEST).json({
         success: false,
         message: "Username Required!",
       });
@@ -19,7 +51,7 @@ export default async function handler(req, res) {
     }
 
     if (!req.body.password) {
-      res.status(400).json({
+      res.status(StatusCodes.BAD_REQUEST).json({
         success: false,
         message: "Password Required!",
       });
@@ -28,7 +60,7 @@ export default async function handler(req, res) {
 
    if(req.body.email){
     if(!validator.isEmail(req.body.email?.value)){
-      res.status(400).json({
+      res.status(StatusCodes.BAD_REQUEST).json({
         success: false,
         message: "Enter a valid Email Address!",
       });
@@ -37,13 +69,30 @@ export default async function handler(req, res) {
    }
 
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const new_refral_code = await generateUniqueToken(8);
+   
+    var userDetails = null;
+    if (refral_code) {
+      const {invalid, user} = await validateRefralCode(refral_code);
+      userDetails = user;
+    }
 
     var createdUser = await UsersModal.create({
       ...req.body,
+      refral_code: new_refral_code,
+      referred_by: userDetails ? userDetails.refral_code: '',
       password: hashedPassword,
     });
 
-    // Generating Access Token
+    if (userDetails) {
+      await RewardsModal.create({
+        referred_by: userDetails._id,
+        referred_to: createdUser._id,
+        amount_type: 'percentage',
+        reward_amount: 5
+      })
+    }
+    
     const AccessToken = await GenAccessToken({
       id: createdUser._id,
     });
@@ -57,11 +106,10 @@ export default async function handler(req, res) {
       })
     );
 
-
-    res.status(201).json({
+    res.status(StatusCodes.CREATED).json({
       success: true,
       message: "Your are Registered Successfully!",
-      data:createdUser
+      data: createdUser
     });
     
   } catch (err) {
@@ -80,13 +128,13 @@ export default async function handler(req, res) {
     console.log(err)
 
     if (requiredFildName) {
-      return res.status(400).json({
+      return res.status(StatusCodes.BAD_REQUEST).json({
         success: false,
         message: `${requiredFildName} is required!`,
       });
     }
 
-    res.status(500).json({
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       success: false,
       message: "Something Went Wrong!",
     });
