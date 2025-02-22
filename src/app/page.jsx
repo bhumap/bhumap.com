74244a79542075945895d335b727f400df4102dc
bhumap <163608301+bhumap@users.noreply.github.com>
2025-02-22 21:11:39 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,6 +20,8 @@ const page = async () => {
   return (
     <div>
       <Hero />
+      <h1>Test</h1>
+    
       <div className="max-w-6xl mx-auto p-4">
         <PropertyCardsGrid
           title="Properties Nearby You"
