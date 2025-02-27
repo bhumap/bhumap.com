7629c4f8576eaf40926f5fbb6deb748e959a5811
psanukul <anukulsaxena@pearlorganisation.com>
2025-02-27 10:10:49 +0530

    fix: homepage listing

--- src/backend/config/dbConnect.js
+++ src/backend/config/dbConnect.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
 const dbConnect = async () => {
+  console.log(" -----------",process.env.DB_URI)
+
   if (mongoose?.connection?.readyState >= 1){
     return
   };
   mongoose.set('debug', true);
+  console.log(" -----------",process.env.DB_URI)
   await mongoose.connect(process.env.DB_URI)
 };
 
